67f091c9375da7100b3e75c5d1853344e34c28d3
Akami-Usagi <akami.3ds@gmail.com>
2023-10-11 22:33:03 -0500

    cambio banner movil

--- src/sections/banner/banner.jsx
+++ src/sections/banner/banner.jsx
@@ -7,7 +7,7 @@ function Banner({banner, changeBanner}) {
 
     return <div className="banner" onClick={()=> changeBanner(!banner)}>
         <img src="/img/banner_coneja.webp" alt="banner" className="banner__image"/>
-        <img src={banner ? "/img/banner_coneja_movil.webp" : "/img/banner_sona.webp"} alt="movilbanner" className="banner__image__movil"/>
+        <img src={banner ? "/img/banner_sona.webp" : "/img/banner_coneja_movil.webp"} alt="movilbanner" className="banner__image__movil"/>
     </div>
 }
 
